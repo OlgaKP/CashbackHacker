87b50726cb3a99617e61555fdf999fcedc791d7f
OlgaKP <89009086+OlgaKP@users.noreply.github.com>
2021-12-08 15:20:11 +0400

    Added a test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -25,4 +25,14 @@ public class CashbackHackServiceTest {
         int expected = 0;
         assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldTestRemainWithoutBuy() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
 }
